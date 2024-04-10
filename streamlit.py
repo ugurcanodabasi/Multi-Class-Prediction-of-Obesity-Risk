ebb3d1791281c30a900bb712b922a4a38dc0255d
asliismailov <157467611+asliismailov@users.noreply.github.com>
2024-04-11 01:20:16 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -20,10 +20,10 @@ def get_pipeline():
     pipeline = joblib.load('lgbm_model_final.pkl')
     return pipeline
 
-main_tab, chart_tab, prediction_tab = st.tabs(["Ana Sayfa", "Grafikler", "Model"])
+main_tab, chart_tab, prediction_tab = st.columns(3)
 
 # Ana Sayfa ########################################################
-if main_tab == "Ana Sayfa":
+if main_tab.button("Ana Sayfa"):
     left_col, right_col = st.columns(2)
 
     left_col.write("""Bu projenin amacı, bireylerde kardiyovasküler hastalıklarla ilişkili obezite riskini tahmin etmek için çeşitli faktörleri kullanmaktır. Kardiyovasküler hastalıklar, dünya genelinde sağlık sorunlarının önde gelen nedenlerinden biri olarak kabul edilmektedir. Bu hastalıkların birçoğu obezite ile doğrudan ilişkilidir. Bu nedenle, obeziteyi öngörmek ve bu konuda farkındalık yaratmak önemlidir.""")
@@ -39,7 +39,7 @@ if main_tab == "Ana Sayfa":
     Kullanıcılar, arayüz üzerinden bireysel özellikleri girebilir ve modele besleyerek obezite risk tahminini alabilirler. Bu tahminler, bireylerin normal kilolu, aşırı kilolu, obez veya aşırı obez olma riskini belirtir.""")
 
 # Grafikler ########################################################
-if chart_tab == "Grafikler":
+if chart_tab.button("Grafikler"):
     col1, col2 = st.columns(2)
 
     with col1:
@@ -51,18 +51,18 @@ if chart_tab == "Grafikler":
         st.image("SHAP.png")
 
 # Tahmin ########################################################
-if prediction_tab == "Model":
-    if st.session_state['tahmin']:
-        model_cont = st.container()
-        model_cont.subheader("Tahmin")
-        col1, col2, col3 = model_cont.columns(3)
-        selected_age = col1.number_input("Yaş")
-        selected_gender = col2.number_input("Cinsiyet")
-        selected_weight = col3.number_input("Kilo")
-        selected_height = col4.number_input("Boy")
-        selected_CH2O = col5.number_input("Günlük su tüketimi")
+if prediction_tab.button("Model"):
+    model_cont = st.container()
+    model_cont.subheader("Tahmin")
+    col1, col2, col3 = model_cont.columns(3)
+    selected_age = col1.number_input("Yaş")
+    selected_gender = col2.number_input("Cinsiyet")
+    selected_weight = col3.number_input("Kilo")
+    selected_height = col4.number_input("Boy")
+    selected_CH2O = col5.number_input("Günlük su tüketimi")
 
-        if col6.button("Tahminle"):
-            prediction = predict_model(df, selected_age, selected_gender, selected_weight, selected_height, selected_CH2O)
-            col6.metric(label="Tahmin Edilen Obezite Riski", value=(prediction[0]))
-            st.balloons()
+    if col6.button("Tahminle"):
+        prediction = predict_model(df, selected_age, selected_gender, selected_weight, selected_height, selected_CH2O)
+        col6.metric(label="Tahmin Edilen Obezite Riski", value=(prediction[0]))
+        st.balloons()
+
