78e77a9c5409a7f42d2ddc45e12c0e5688742e93
asliismailov <157467611+asliismailov@users.noreply.github.com>
2024-04-11 01:06:30 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -22,37 +22,45 @@ def get_pipeline():
 
 main_tab, chart_tab, prediction_tab = st.tabs(["Ana Sayfa", "Grafikler", "Model"])
 
-if main_tab == "Ana Sayfa":
-    left_col, right_col = main_tab.columns(2)
+# Ana Sayfa ########################################################
 
-    left_col.write("""Bu projenin amacı, bireylerde kardiyovasküler hastalıklarla ilişkili obezite riskini tahmin etmek için çeşitli faktörleri kullanmaktır. Kardiyovasküler hastalıklar, dünya genelinde sağlık sorunlarının önde gelen nedenlerinden biri olarak kabul edilmektedir. Bu hastalıkların birçoğu obezite ile doğrudan ilişkilidir. Bu nedenle, obeziteyi öngörmek ve bu konuda farkındalık yaratmak önemlidir.""")
+left_col, right_col = main_tab.columns(2)
 
-    left_col.write("""Veri Seti ve Hedef
-    Bu projede kullanılan veri seti, bireylerin demografik bilgilerini, yaşam tarzı alışkanlıklarını ve fizyolojik ölçümlerini içerir. Ölçümler arasında boy, kilo, günlük su tüketimi, fiziksel aktivite düzeyi gibi faktörler bulunmaktadır. Veri setindeki her bir satır, bir bireyi temsil eder ve bu bireylerin obezite durumları "NObeyesdad" sütununda belirtilmiştir.""")
+left_col.write("""Bu projenin amacı, bireylerde kardiyovasküler hastalıklarla ilişkili obezite riskini tahmin etmek için çeşitli faktörleri kullanmaktır. Kardiyovasküler hastalıklar, dünya genelinde sağlık sorunlarının önde gelen nedenlerinden biri olarak kabul edilmektedir. Bu hastalıkların birçoğu obezite ile doğrudan ilişkilidir. Bu nedenle, obeziteyi öngörmek ve bu konuda farkındalık yaratmak önemlidir.""")
 
-    #TAVSİYE:Veri setinin bir kısmı eklenebilir
+left_col.write("""Veri Seti ve Hedef
+Bu projede kullanılan veri seti, bireylerin demografik bilgilerini, yaşam tarzı alışkanlıklarını ve fizyolojik ölçümlerini içerir. Ölçümler arasında boy, kilo, günlük su tüketimi, fiziksel aktivite düzeyi gibi faktörler bulunmaktadır. Veri setindeki her bir satır, bir bireyi temsil eder ve bu bireylerin obezite durumları "NObeyesdad" sütununda belirtilmiştir.""")
 
-    right_col.write("""Kullanılan Algoritmalar
-    Bu proje, LightGBM makine öğrenimi modeli kullanmaktadır. LightGBM, yüksek performanslı ve hızlı bir gradyan arttırma (gradient boosting) algoritmasıdır. Bu algoritma, veri setindeki örüntüleri öğrenerek ve karmaşık ilişkileri modelleyerek obezite riskini tahmin etmek için kullanılır.""")
+#TAVSİYE:Veri setinin bir kısmı eklenebilir
 
-    right_col.write("""Uygulama: Streamlit ile Model Tahmini
-    Bu projede, geliştirilen modelin kullanıcı dostu bir arayüz ile sunulması amaçlanmıştır. Streamlit adlı Python kütüphanesi, basit ve etkileşimli web uygulamaları oluşturmayı sağlar. Bu projede, geliştirilen LightGBM modeli Streamlit arayüzü ile entegre edilmiştir.
-    Kullanıcılar, arayüz üzerinden bireysel özellikleri girebilir ve modele besleyerek obezite risk tahminini alabilirler. Bu tahminler, bireylerin normal kilolu, aşırı kilolu, obez veya aşırı obez olma riskini belirtir.""")
+right_col.write("""Kullanılan Algoritmalar
+Bu proje, LightGBM makine öğrenimi modeli kullanmaktadır. LightGBM, yüksek performanslı ve hızlı bir gradyan arttırma (gradient boosting) algoritmasıdır. Bu algoritma, veri setindeki örüntüleri öğrenerek ve karmaşık ilişkileri modelleyerek obezite riskini tahmin etmek için kullanılır.""")
 
-if chart_tab == "Grafikler":
-    col1, col2 = chart_tab.columns(2)
+right_col.write("""Uygulama: Streamlit ile Model Tahmini
+Bu projede, geliştirilen modelin kullanıcı dostu bir arayüz ile sunulması amaçlanmıştır. Streamlit adlı Python kütüphanesi, basit ve etkileşimli web uygulamaları oluşturmayı sağlar. Bu projede, geliştirilen LightGBM modeli Streamlit arayüzü ile entegre edilmiştir.
+Kullanıcılar, arayüz üzerinden bireysel özellikleri girebilir ve modele besleyerek obezite risk tahminini alabilirler. Bu tahminler, bireylerin normal kilolu, aşırı kilolu, obez veya aşırı obez olma riskini belirtir.""")
 
-    with col1:
-        st.header("Korelasyon Matrisi")
-        st.image("korelasyon.png")
+#TAVSİYE: IMAGE EKLENEBİLİR
+#right_col.image("spoti.jpg")
 
-    with col2:
-        st.header("Shap")
-        st.image("SHAP.png")
+# Grafikler ########################################################
 
-if prediction_tab == "Model":
+import streamlit as st
+
+col1, col2 = st.columns(2)
+
+with col1:
+   st.header("Korelasyon Matrisi")
+   st.image("korelasyon.png")
+
+with col2:
+   st.header("Shap")
+   st.image("SHAP.png")
+
+#Tahmin ########################################################
+
+if __name__ == '__main__':
     if st.session_state['tahmin']:
-        model_cont = prediction_tab.container()
         model_cont.subheader("Tahmin")
         col1, col2, col3 = model_cont.columns(3)
         selected_age = col1.number_input("Yaş")
